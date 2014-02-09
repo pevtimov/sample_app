f0f530abc4c06df9e21ae6942b11a1ff797ff0fa
pevtimov <p.evtimov@outlook.com>
2014-02-09 13:01:00 +0800

    Added a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis
+	# When a given page provides no title then we return the base_title only
+	# If a page does provide a title, then we concatenate base_title & page_title
+	# The page title is passed to the method via the page_title argument
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,12 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
 		end
 	end
 
@@ -37,6 +42,19 @@ describe "StaticPages" do
 	    it "should have the right title" do
 			visit '/static_pages/about'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
