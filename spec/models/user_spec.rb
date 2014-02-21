3c17508e75c83f4504dd2174b9b692e7b766bd72
pevtimov <p.evtimov@outlook.com>
2014-02-21 23:20:31 +0800

    Added test for email with mixed case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,6 +73,16 @@ describe User do
 		it { should_not be_valid }
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@EXAmple.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
 	#test for blank password
 	describe "when password is not present" do
 		
